f496fffb84d3bd256ed4f81241702e4958b76822
Tristan Jet <tristanjetlay1@gmail.com>
2025-08-19 14:04:15 +0300

    edit

--- mod02/06-rand-combination.py
+++ mod02/06-rand-combination.py
@@ -1,18 +1,18 @@
 import random
 
-seq1 = []
-seq2 = []
+seq1 = [0, 0, 0]
+seq2 = [0, 0, 0, 0]
 
 i = 0
 while i < 3 :
-    seq1.append(random.randrange(0, 10))
+    seq1[i] = random.randrange(0, 10)
     i+=1
 
 print(f"Code 1: {seq1}")
 
 i = 0
 while i < 4 :
-    seq2.append(random.randrange(1, 7))
+    seq2[i] = random.randrange(1, 7)
     i+=1
 
 print(f"Code 2: {seq2}")
--- mod03/4-leapyears.py
+++ mod03/4-leapyears.py
@@ -2,15 +2,14 @@ import sys
 
 yr = int(input("Enter year: "))
 
-remfour = yr % 4
 remcent = yr % 100
-
 if remcent == 0:
     remfh = yr % 400
     if remfh == 0:
         print("Leap year!")
         sys.exit()
 
+remfour = yr % 4
 if remfour == 0:
     if remcent != 0:
         print("Leap year!")
