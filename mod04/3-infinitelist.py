474930151d85fcddb9346991d8e72c5dbd932580
Tristan Jet <tristanjetlay1@gmail.com>
2025-09-06 21:45:18 +0300

    small fix

--- mod04/3-infinitelist.py
+++ mod04/3-infinitelist.py
@@ -4,7 +4,7 @@ while 1:
         x = int(input("Enter a number to add, string to exit: "))
         ls.append(x)
     except ValueError:
-        if len(ls) > 2:
+        if len(ls) > 1:
             ls.sort()
             print(f"Smallest inputted: {ls[0]}\nLargest inputted: {ls[-1]}")
         else:
